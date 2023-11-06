03d9d1f7f7db1973d3297c6f272da9921fbf5ae7
akram7737khan <akramk@domainexpertsgroup.com>
2023-11-06 07:48:36 +0530

    changes

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,6 +3,6 @@ const BASE_URL = 'http://{Network IP address}:4000'
 export const API = BASE_URL + '/api/v1/'
 
 export const endpoints = {
-    posts: API + 'posts',
+    posts: API + 'posts?_sort=desc',
     addPost: API + 'posts'
 }
