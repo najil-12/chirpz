9d9c78c16fb1ecea11e1f5d2c7e082c7b7b4bd08
akram7737khan <akramk@domainexpertsgroup.com>
2023-11-06 12:23:16 +0530

    changes

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://{Network IP address}:4000'
+const BASE_URL = 'http://192.168.1.15:4000'
 
 export const API = BASE_URL + '/api/v1/'
 
 export const endpoints = {
-    posts: API + 'posts?_sort=desc',
+    posts: API + 'posts?_sort=createdAt&_order=desc',// For now I added static params only
     addPost: API + 'posts'
 }
